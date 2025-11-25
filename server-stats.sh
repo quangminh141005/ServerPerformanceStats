63aea6bb8f703b6c57c5e253ba02bc7a9943f9e1
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2025-11-25 20:39:46 +0700

    add cpu usage

---
+++ server-stats.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+
+# server performance stats
+
+set -o errexit # stop if command fail 
+set -o nounset # stop if i use a unset varivable
+set -o pipefall # fail if part of a pipeline fail
+
+# seperator line
+hr() {
+    printf '\n==============================================================\n\n'
+}
+
+title() {
+    hr
+    echo ">>> $1"
+    hr
+}
+
+cpu_usage() {
+    title "cpu usage"
+
+    if command -v top >/dev/null 2>&1; then
+        cpu_line=$(LC_ALL=C top -bn1 | grep -m1 "Cpu(s)") # LC_ALL=C: output always English, b(non interative), n1(one iteration), m1(stop after the first match)
+        # Extract idle % then subtract from 100
+        cpu_used=$(echo "$cpu_line" | awk -F',' '
+        {
+            idle=0
+            for (i=1; i<=NF; i++) {
+                if ($i ~ /id/) {
+                    # take the number before "id"
+                    split($i, a, " ") # a is the array to store the variable after split
+                    for (j=1; j<length(a); j++) {
+                        if (a[j] ~ /^[0-9.]+$/)
+                        idle=a[j]
+                    }
+                }
+            }
+            if (idle == 0) idle = 0
+            printf "%.2f\n", 100-idle
+        }')
+
+        echo "Total CPU usage: "${cpu_used}"
+}
